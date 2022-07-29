a1178839ae6daefe38ae3cdf7de7a809d465f23e
18520426 <63585312+18520426@users.noreply.github.com>
2022-07-29 11:22:48 +0700

    add codes

--- src/backend/main/views.py
+++ src/backend/main/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from .ai import predict, get_metadata, detect_cough
+from .ai import predict, get_metadata, detect_cough, convert_webm_to_wav
+import os
 import json
 
 # Create your views here.
@@ -9,12 +9,30 @@ import json
 def main(request):
     if request.method != "POST":
         return HttpResponse("NOT FOUND")
+
     metadata = get_metadata(request.POST)
-    cough = detect_cough(request.FILES["audio_1"])
+    input_file = request.FILES["audio"]
+    audio_1 = request.FILES["audio_1"]
+
+    try:
+        print(audio_1)
+        print(type(audio_1))
+        cough = detect_cough(audio_1)
+    except:
+        url_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),f"weights/recorded/")
+        url_save = os.path.join(os.path.dirname(os.path.dirname(__file__)), f"weights/input_file/")
+        convert_webm_to_wav(str(request.FILES["audio_1"]),url_path,url_save)
+        input_file = os.path.join(url_save, 'soundwebm.wav')
+        print(input_file)
+        print(type(input_file))
+        cough = detect_cough(input_file)
 
     if cough > 0.5:
-        res = predict(request.FILES["audio"],metadata)
+        print(input_file)
+        print(type(input_file))
+        res = predict(input_file,metadata)
         response = {"prob": "Xác xuất dương tính là: "+str(res[0])}
     else:
-        response = {"prob": "Không thể xác định âm thanh ho. Vui lòng thử lại. ("+str(cough)+")"}
+        response = {"prob": "Không thể xác định âm thanh ho. Vui lòng thử lại"}
+
     return HttpResponse(json.dumps(response), content_type="application/json")
